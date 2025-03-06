90564e2ff7c97396b7309ee7b4d994413815d281
steven <shotta3xd@gmail.com>
2025-03-06 12:39:03 -0500

    added current working team verison of project :

--- pokeasync/app.js
+++ pokeasync/app.js
@@ -2,9 +2,13 @@
 
 // This function make a api request the the endpoint specifed and returns that data.
 
+let pokemon_team_state = [];
+
 const button = document.querySelector("button");
 const rock = document.querySelector("#stone");
 const pokemon = document.querySelector("img");
+const catchbtn = document.querySelector("#catch");
+const team = document.querySelector(".teamcontainer");
 
 button.addEventListener("click", async () => {
   const value = await apiRequest();
@@ -16,6 +20,12 @@ rock.addEventListener("click", async () => {
   const val = await apiRequest();
   console.log(val);
   pokemon.src = "";
+});
+
+catchbtn.addEventListener("click", () => {
+  pokemon_team_state.push(pokemon.src);
+  addPokemonDom();
+  console.log(pokemon_team_state);
 });
 // task create a event listener that grabs the rock !
 
@@ -30,6 +40,24 @@ async function apiRequest() {
   }
 }
 
+function addPokemonDom() {
+  team.innerHTML = "";
+  pokemon_team_state.forEach((src) => {
+    const img = document.createElement("img");
+    img.src = src;
+    console.log(src);
+    team.append(img);
+  });
+}
+
+// tools helper function
 const randomize = () => {
   return Math.floor(Math.random() * 900 + 1);
 };
+
+// assignment
+// create a constraint that only allows 6 pokemon in the team
+// add styling that wraps the element to the new line.
+// Create a container for your opponent
+// every pokemon you do not select your opponent gets.
+// render your opponents team right next to your team.
